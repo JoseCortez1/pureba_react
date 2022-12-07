2b86e0687de1dfd0643e62bb0e3ca7d1912d7bd4
JoseCortez1 <jose.vazcortez@gmail.com>
2022-12-07 08:02:03 -0600

    error css

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Error = () => {
-	return <div className="error_container">Error</div>;
+	return (
+		<div className="error_container">
+			<p>Error</p>
+			<p>Please try again later or reload the page.</p>
+		</div>
+	);
 };
 
 export default Error;
